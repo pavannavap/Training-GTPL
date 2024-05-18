bc757f63d9c5d3ee27aff42e89cdd6ded9ba39db
pavanandu <pavankumarmallepoola@gmail>
2024-05-18 08:38:36 +0530

    commited

--- prime_numbers.py
+++
@@ -1,9 +0,0 @@
-# to find a prime number
-num = int(input("enter a number to check it is prime number or not :- "))
-
-for i in range(2,num):
-    if num % i == 0:
-        print("not prime number")
-        break
-else:
-    print("Given number is prime number")
